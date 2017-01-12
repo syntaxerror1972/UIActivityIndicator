ad53476a26fd06a5b451804d91d37c1295bd3af5
Shrawan Shinde <shrawanshinde@Shrawans-MacBook-Pro.local>
2017-01-13 02:03:43 +0530

    Use an activity indicator to show that a task is in progress. An activity indicator appears as a “gear” that is either spinning or stopped.

--- UIActivityIndicator/ViewController.swift
+++ UIActivityIndicator/ViewController.swift
@@ -9,11 +9,41 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    var activityIndicator = UIActivityIndicatorView();
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        self.view.backgroundColor = UIColor.gray
+     
+         activityIndicator = UIActivityIndicatorView(activityIndicatorStyle: .whiteLarge)
+        activityIndicator.center = self.view.center
+        activityIndicator.startAnimating()
+        
+        self.view.addSubview(activityIndicator)
     }
+    
+    //use when required call directly this function 
+    
+    func startLoading(){
+        activityIndicator.center = self.view.center;
+        activityIndicator.hidesWhenStopped = true;
+        activityIndicator.activityIndicatorViewStyle = UIActivityIndicatorViewStyle.gray;
+        view.addSubview(activityIndicator);
+        
+        activityIndicator.startAnimating();
+        UIApplication.shared.beginIgnoringInteractionEvents();
+        
+    }
+    
+    func stopLoading(){
+        
+        activityIndicator.stopAnimating();
+        UIApplication.shared.endIgnoringInteractionEvents();
+        
+    }
+
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
